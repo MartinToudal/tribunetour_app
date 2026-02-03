1befdf990ebabb83cd023f9dc04bb21a6dfd162a
Martin Toudal <martin@toudal.dk>
2026-02-03 21:07:06 +0100

    Initial Commit

---
+++ Tribunetour/TribunetourApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribunetourApp.swift
+//  Tribunetour
+//
+//  Created by Martin Toudal on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TribunetourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
